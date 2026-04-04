1151d5071e285403d8bb15cdd4ba149b032dd4ed
Himani Lohani <himanilohani68@gmail.com>
2026-04-04 10:44:58 +0530

    added location input and search button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🚑MediFlow</h1>
+<p>Find and book the nearest ambulance in real-time</p>
+<button>Book Ambulance</button>
     </div>
   );
 }
